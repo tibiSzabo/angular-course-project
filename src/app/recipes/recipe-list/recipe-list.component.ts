9f8ce4fba0a5125ad0b5cfa9a2eb9edb43f516ce
tibiSzabo <bukkfabviszti@gmail.com>
2018-11-24 16:10:50 +0100

    cross component communication completed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,22 +8,12 @@ import {Recipe} from './recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe [] = [];
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe [] = [
-    new Recipe('Test Recipe', 'Test Recipe Description',
-      'https://media.defense.gov/2017/Jul/14/2001778344/780/780/0/170713-F-BI157-009.JPG'),
-    new Recipe('Another Recipe', 'Another Recipe Description',
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,36 @@
-export class RecipeService {
+import {Recipe} from './recipe-list/recipe.model';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service.';
 
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe [] = [
+
+    new Recipe(
+      'Test Recipe',
+      'Test Recipe Description',
+      'https://media.defense.gov/2017/Jul/14/2001778344/780/780/0/170713-F-BI157-009.JPG',
+      [new Ingredient('Coffee', 1), new Ingredient('Sprite', 20)]
+    ),
+
+    new Recipe(
+      'Another Recipe',
+      'Another Recipe Description',
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg',
+      [new Ingredient('Cat', 3), new Ingredient('Dog', 21)]
+    )
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) { }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
 }
